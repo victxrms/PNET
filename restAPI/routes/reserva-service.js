4512c4516f38db8020d0a935bc3a191f9f1071f2
Ascastillo98 <64729683+Ascastillo98@users.noreply.github.com>
2023-04-01 12:39:51 +0200

    Add files via upload

---
+++ restAPI/routes/reserva-service.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+let db;
+let ObjectId = require('mongodb').ObjectId;
+const Reservas = function () {
+};
+
+Reservas.prototype.connectDb = function (callback) {
+    MongoClient.connect("mongodb+srv://testPNET:testPNET123@cluster0.mfjkdgc.mongodb.net/?retryWrites=true&w=majority",
+        {useNewUrlParser: true, useUnifiedTopology: true},
+        function (err, database) {
+            if (err) {
+		console.log(err);
+                callback(err);
+            }
+
+   	    db = database.db('PNET-reservas').collection('reservas');
+	    console.log("Conexión correcta");
+
+            callback(err, database);
+        });
+};
---
+++ restAPI/routes/reservas.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const reservasService = require('./reserva-service');
+
+
+router.get('/', function (req, res) {
+    reservasService.getAll((err, reservas) => {
+            if (err) {
+                res.status(500).send({
+                    msg: err
+                });
+            } else if (reservas.length == 0){
+            	res.status(500).send({
+                    msg: "no existen reservas"
+                });
+            } else {
+                res.status(200).send(reservas);
+            }
+        }
+    );
+});
+
+router.delete('/:_id', function (req, res) {
+    let _id = req.params._id;
+    reservasService.remove(_id, (err) => {
+            if (err) {
+                res.status(500).send({
+                	msg: err
+            	});
+            } else {
+                res.status(200).send({
+                    msg: 'La reserva ha sido borrada'
+                });
+            }
+        }
+    );
+});
+
+module.exports = router;
