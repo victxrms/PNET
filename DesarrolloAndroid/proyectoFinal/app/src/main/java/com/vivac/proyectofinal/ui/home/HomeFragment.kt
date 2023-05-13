c7e743713e3d8dedb848060c68a371e413503762
Victor Moreno <vmoso2002@gmail.com>
2023-05-13 10:55:20 +0200

    fondo home actualiza con modo noche y modo dia + acabado banner y modonoche

--- DesarrolloAndroid/proyectoFinal/app/src/main/java/com/vivac/proyectofinal/ui/home/HomeFragment.kt
+++ DesarrolloAndroid/proyectoFinal/app/src/main/java/com/vivac/proyectofinal/ui/home/HomeFragment.kt
@@ -3,13 +3,16 @@ package com.vivac.proyectofinal.ui.home
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.vivac.proyectofinal.R
 import com.vivac.proyectofinal.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,6 +22,9 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var mainLayout: ConstraintLayout
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +41,19 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        mainLayout = binding.layouthome
+
+        val dayNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        when (dayNightMode) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                mainLayout.setBackgroundResource(R.drawable.fondodia)
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                mainLayout.setBackgroundResource(R.drawable.fondonoche)
+            }
+        }
+
         return root
     }
 
