7a7f2017580ae0a4eeb8eed645f45b68863c8f42
Victor Moreno <vmoso2002@gmail.com>
2023-05-19 10:45:31 +0200

    ahora bien el get

--- DesarrolloAndroid/proyectoFinal/app/src/main/java/com/vivac/proyectofinal/ui/reservas/ReservasFragment.kt
+++ DesarrolloAndroid/proyectoFinal/app/src/main/java/com/vivac/proyectofinal/ui/reservas/ReservasFragment.kt
@@ -8,12 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
-<<<<<<< HEAD
-=======
-import androidx.lifecycle.lifecycleScope
->>>>>>> ae9bf2f2cf13b83f6835a672a3d2fe5492b9fd59
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vivac.proyectofinal.databinding.FragmentReservasBinding
@@ -22,40 +17,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-<<<<<<< HEAD
-=======
-import com.vivac.proyectofinal.ui.salas.ReservaAdapter
-import com.vivac.proyectofinal.ui.salas.SalaAdapter
-import com.vivac.proyectofinal.ui.salas.SalasViewModel
-import io.ktor.client.*
-import io.ktor.client.engine.android.*
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.request.*
-import io.ktor.http.ContentType.Application.Json
-import kotlinx.serialization.json.Json
->>>>>>> ae9bf2f2cf13b83f6835a672a3d2fe5492b9fd59
 
 class ReservasFragment : Fragment() {
 
     private var _binding: FragmentReservasBinding? = null
 
-<<<<<<< HEAD
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-=======
-    private lateinit var viewModel: ReservasViewModel
-    private lateinit var recyclerView: RecyclerView
-
-    private val reservasList: MutableLiveData<List<Reserva>> = MutableLiveData<List<Reserva>>().apply {
-        value = emptyList()
-    }
-    private var listaReservas: List<Reserva> = emptyList()
-    private var adapter =  ReservaAdapter(listaReservas)
-
-
->>>>>>> ae9bf2f2cf13b83f6835a672a3d2fe5492b9fd59
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -68,7 +37,6 @@ class ReservasFragment : Fragment() {
         _binding = FragmentReservasBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-<<<<<<< HEAD
         var reservas : List<Reserva>
         val recyclerView: RecyclerView = binding.recyclerView
         val context = requireContext()
@@ -79,52 +47,7 @@ class ReservasFragment : Fragment() {
             val adapter = ReservaAdapter(reservas)
             withContext(Dispatchers.Main){
                 recyclerView.adapter = adapter
-=======
-            val client = HttpClient(Android) {
-                // Agregamos el módulo de serialización JSON
-                install(JsonFeature) {
-                    serializer = KotlinxSerializer()
-                }
             }
-            // Hacemos la solicitud GET utilizando el cliente
-
-            val response: List<Reserva> =  client.get("http://127.0.0.1:8080/reservas")
-
-            withContext(Dispatchers.Main) {
-                listaReservas = response
-                adapter = ReservaAdapter(listaReservas)
-                adapter.setReservas(listaReservas)
-                reservasList.value = listaReservas
-                viewModel.getlist(reservasList)
-            }
-
-        }
-        return rootView
-    }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-
-            viewModel = ViewModelProvider(this).get(ReservasViewModel::class.java)
-            adapter.setReservas(listaReservas) //no está inicializada
-            reservasList.value = listaReservas
-            viewModel.getlist(reservasList)
-
-
-
-            viewModel.getReservas().observe(viewLifecycleOwner) { listaReservas ->
-                adapter.setReservas(listaReservas)
-                adapter.notifyDataSetChanged()
-
->>>>>>> ae9bf2f2cf13b83f6835a672a3d2fe5492b9fd59
-            }
-
-            recyclerView = view.findViewById(R.id.recyclerViewR)
-            adapter = ReservaAdapter()
-
-            recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            recyclerView.adapter = adapter
-
         }
         return root
     }
--- DesarrolloAndroid/proyectoFinal/app/src/main/java/com/vivac/proyectofinal/ui/reservas/ReservasViewModel.kt
+++ DesarrolloAndroid/proyectoFinal/app/src/main/java/com/vivac/proyectofinal/ui/reservas/ReservasViewModel.kt
@@ -4,24 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-<<<<<<< HEAD
 class ReservasViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is Reservas Fragment"
     }
     val text: LiveData<String> = _text
-=======
-class ReservasViewModel() : ViewModel() {
-    private lateinit var reservasLiveData: MutableLiveData<List<Reserva>>
-
-    fun getlist(lista: MutableLiveData<List<Reserva>>){
-        reservasLiveData = lista
-    }
-
-    fun getReservas(): LiveData<List<Reserva>> {
-        return reservasLiveData
-    }
-
->>>>>>> ae9bf2f2cf13b83f6835a672a3d2fe5492b9fd59
 }
